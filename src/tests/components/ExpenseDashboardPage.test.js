eb78b2ac7d7b9b5b4864402b05aa6c7ad5a03020
Cortes <sergiocortes1@kpmg.com.mx>
2018-09-07 16:44:51 -0500

    Initial commit

---
+++ src/tests/components/ExpenseDashboardPage.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import ExpenseDashboardPage from '../../components/ExpenseDashboardPage';
+
+// react-test-renderer
+test('should render ExpenseDashboardPage correctly',()=>{
+    const wrapper = shallow(<ExpenseDashboardPage />);
+    expect(wrapper).toMatchSnapshot();
+});
